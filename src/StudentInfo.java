5d3415693504a760eb8f8323aab6872c8932681e
febstar <febechukwuonyeyili@gmail.com>
2023-10-09 18:55:47 +0100

    Initial commit

---
+++ src/StudentInfo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class StudentInfo {
+    public static void main(String[]args) {
+        int sumOfAges = 0;
+        String[] names = new String[10];
+        int[] ages = new int[10];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter the name of student" + (i + 1) + ":");
+            names[i] = scanner.nextLine();
+
+            System.out.print("Enter the age of student" + (i+1) + ":");
+            ages[i] = scanner.nextInt();
+            scanner.nextLine();
+
+            sumOfAges += ages[i];
+        }
+
+        double averageAge = (double)sumOfAges/10;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(names[i] + ":"+ ages[i]+ "years");
+        }
+
+        System.out.println("\nSum of Ages: " + sumOfAges + " years");
+        System.out.println("Average Age: " + averageAge + " years");
+
+        scanner.close();
+    }
+}
